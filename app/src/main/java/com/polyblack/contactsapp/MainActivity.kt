d5695b2bf1b58c188ea9484ada4f8e24ca92d9ec
Polina Shirobokova <izhevsk07@gmail.com>
2020-10-26 19:29:23 +0400

    добавлен UI

--- app/src/main/java/com/polyblack/contactsapp/MainActivity.kt
+++ app/src/main/java/com/polyblack/contactsapp/MainActivity.kt
@@ -7,5 +7,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            setContactListFragment()
+        }
+    }
+
+    private fun setContactListFragment() {
+        val contactListFragment = ContactListFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, contactListFragment)
+            .commit()
+    }
+
+    private fun setContactDetailsFragment() {
+        val detailsFragment = ContactDetailsFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, detailsFragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
